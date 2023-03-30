625feaa773fddfce05b758292d3a4c65d4edd034
Sungho Lim <sungho.lim@wesang.com>
2023-03-31 01:55:11 +0900

    chore: Set typescript

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,3 +16,18 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
+  })
+}
